c0d8750acd37ef83abc3eb0f125520cb7eaad292
Will Shackleton <w.shackleton@gmail.com>
2016-08-30 17:07:08 +0100

    Added AST format for SMT-LIB parenthesis format

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,5 +2,10 @@ module Lib
     ( someFunc
     ) where
 
+import Lib.SmtLang.Base
+
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = putStrLn $ prettyPrint $ [
+    Paren [Name ">", Name "1", Paren [Name "+", Name "1", Name "2"]],
+    Paren [Name "=", Paren [Name "+", Name "y", Name "z"], Name "x"]
+    ]
